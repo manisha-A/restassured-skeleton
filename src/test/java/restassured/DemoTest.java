dfb6befce38f152cf4101f5f4db69059e4e5c73e
Manisha Awasthi <itsmanisha_15@yahoo.co.in>
2026-08-03 11:38:41 +0100

    added aprametrised test

--- src/test/java/restassured/DemoTest.java
+++ src/test/java/restassured/DemoTest.java
@@ -3,12 +3,15 @@ package restassured;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.Map;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DemoTest {
     static final String BASE_URL = "https://api.github.com";
+    static final String SEARCH_EP = "https://api.github.com/search/repositories";
     @Test
     void someTest(){
         System.out.println("test");
@@ -39,5 +42,35 @@ public class DemoTest {
                 .header("etag", notNullValue());
     }
 
+    @Test
+    void canPassParams(){
+        RestAssured
+                .given()
+                .params(Map.of("q", "java", "per_page", "1"))
+                .get(BASE_URL + "/search/repositories")
+                .then()
+                .statusCode(200);
+    }
 
-}
+    @ParameterizedTest
+    @MethodSource("paramsHashMap")
+    void parameterisedTest(Map<String, String> params, int expectedCount){
+        System.out.println(params.entrySet());
+        var response =
+        RestAssured
+                .given()
+                .params(params)
+                .get(SEARCH_EP)
+                .jsonPath();
+        assertEquals(response.getInt("items.size()"),expectedCount);
+    }
+
+    static Object[][] paramsHashMap () {
+        return new Object[][]{
+                {Map.of("q", "java", "per_page", "1"), 1},
+                {Map.of("q", "java", "per_page", "2"), 2}
+        };
+        };
+    }
+
+
